81eabd7bdac6026f86764a5714767803d3ebf919
leoole100 <22222725+leoole100@users.noreply.github.com>
2022-06-20 17:54:17 +0200

    added substitution

--- blatt05/poisson2.c
+++ blatt05/poisson2.c
@@ -1,38 +1,46 @@
+/*
+ * Exercise 5a) with substitution of r
+*/
+
 #include <stdio.h>
 #include <math.h>
 
 
-#define H 0.001
-#define rstart 100
+const double H = 1.e-4;
 
-
-double rho(double r);
-void numerov(double* y1, double* y2, double r);
+double rho(double x);
+double dr(double x);
+void numerov(double* y1, double* y2, double* x);
 
 
 int main(){
-    
-    double y1=0, y2 = 0;
-    
-    for(double r = rstart; r > 0; r -= H){
-        numerov(&y1, &y2, r);
-        printf("%f\n", y2);
-    }
-    
-    // Ergebnis:                        -0.039789
-    // Analytisch: -1/(8 pi) \approx    -0.0397887
-    
-    return 0;
+	double y1=0, y2 = 0;
+	
+	// substitute r to x = x/(x-1)^1 => dr = - (1+x)/(x-1)^3 
+	for(double x = 0; x < 1; x += H){ // and reversed direction
+		numerov(&y1, &y2, &x);
+		printf("%f\n", -1. * y2);
+	}
+	
+	// Ergebnis:                        -0.039789
+	// Analytisch: -1/(8 pi) \approx    -0.0397887
+	
+	return 0;
 }
 
 
-double rho(double r){
-    double wert = exp(-r)/(8 * M_PI);
-    return wert;
+double rho(double x){
+	return exp(- x/powf(x-1, 2.))/(8 * M_PI);
 }
 
-void numerov(double* y1, double* y2, double r){
-    double y = 2 * *y2 - *y1 - H*H/12*(rho(r+H)+10*rho(r)+rho(r-H));
-    *y1 = *y2;
-    *y2 = y;
+double dr(double x){
+	return -(1 + x)/powf(x-1, 3.);
+}
+
+
+void numerov(double *y1, double *y2, double *x){
+	double y = 2 * *y2 - *y1 - powf(H, 2.)/12 * (dr(*x+H)*rho(*x+H) + 10*dr(*x)*rho(*x) + dr(*x-H)*rho(*x-H));			// 0.027310
+	//double y = 2 * *y2 - *y1 - dr(*x) * powf(H, 2.)/12 * (rho(*x+H) + 10*rho(*x) + rho(*x-H));							// 0.027310
+	*y1 = *y2;
+	*y2 = y;
 }
