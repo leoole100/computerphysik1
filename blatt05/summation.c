75ddf1fe1d1fedcdcf5605c9a8edadb4158bc7b9
leoole100 <22222725+leoole100@users.noreply.github.com>
2022-06-27 14:47:24 +0200

    random commit

--- blatt05/summation.c
+++ blatt05/summation.c
@@ -16,14 +16,15 @@ void numerov(double* y1, double* y2, double* x);
 
 
 int main(){
-	double y1=0, y2 = 0;
-	file = fopen("./poisson2.dat", "w+");
+	double y=0;
+	file = fopen("./summation.dat", "w+");
 	
 	// substitute r to x = x/(x-1)^1 => dr = - (1+x)/(x-1)^3 
 	for(double x = 0; x < 1; x += H){ // and reversed direction
-		numerov(&y1, &y2, &x);
+		y += dr(x) * rho(x) * H;
+		fprintf(file, "%f %f\n", x, y);
 	}
-
+	
 	// Ergebnis:                        -0.039789
 	// Analytisch: -1/(8 pi) \approx    -0.0397887
 	
@@ -38,15 +39,4 @@ double rho(double x){
 
 double dr(double x){
 	return -(1 + x)/powf(x-1, 3.);
-}
-
-
-void numerov(double *y1, double *y2, double *x){
-	double y = 2 * *y2 - *y1 - powf(H, 2.)/12 * (dr(*x+H)*rho(*x+H) + 10*dr(*x)*rho(*x) + dr(*x-H)*rho(*x-H));			// -0.027310
-	//double y = 2 * *y2 - *y1 - dr(*x) * powf(H, 2.)/12 * (rho(*x+H) + 10*rho(*x) + rho(*x-H));							// -0.027310
-	*y1 = *y2;
-	*y2 = y;
-
-	printf("%f %f %f \n", *x, *y2, dr(*x));
-	fprintf(file, "%f %f %f \n", *x, *y2, dr(*x));
 }
