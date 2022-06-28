0746be4bef1f168129d7f7b0ab7a51db9ff6f9d9
Leon Oleschko <22222725+leoole100@users.noreply.github.com>
2022-06-28 15:52:33 +0200

    funktionierende bisektion

--- blatt05/schroedinger.c
+++ blatt05/schroedinger.c
@@ -4,35 +4,35 @@
 #include <stdbool.h>
 
 #define H 1.e-3
-#define XMAX 10
+#define XMAX 10.
+#define N ((int) (XMAX/H))
 
 #define UNCERT_END 1.e-10
 
 #define UNCERT_GUESS 0.2
 double guess[] = {2.5, 4.5, 6, 7.5, 8.75, 10, 12, 14, 16, 18.5};
 
-double integ(double E, bool save);
+void integ(double E, double *phi);
 void numerov(double *y1, double *y2, double x, double E);
 void findAndExport(int n);
 double V(double x);
 
-FILE *file;
+
 
 int main(){
     
-    file = fopen("schroedinger.dat", "w+");
+    for (size_t i = 0; i < sizeof(guess)/ sizeof(guess[0]); i++){
+		findAndExport(i);
+	}
+	
+	
     
-    
-	findAndExport(0);
-    
-    
-    
-    fclose(file);
     
     return 0;
 }
 
 void findAndExport(int n){
+	double phi[N];
 
 	// bisection to find the root
 	double E0 = guess[n]-UNCERT_GUESS;
@@ -41,9 +41,11 @@ void findAndExport(int n){
 	do {
 		Ex = (E1+E0)/2.0;
 		
-		tmp = integ(Ex,false);
-	
-		if(tmp*integ(E0,false)>0)
+		integ(Ex,phi);
+		tmp = phi[N-1];
+		integ(E0,phi);
+
+		if(tmp*phi[N-1]>0)
 			E0=Ex;
 		else
 			E1=Ex;
@@ -51,19 +53,36 @@ void findAndExport(int n){
 
 	printf("guess: %.2f E: %f\n", guess[n], E1);
 
+	// normalizing
+	double norm = 0;
+	for(int i=0; i<N; i++){
+		norm += phi[i]*phi[i]*H;
+	}
+	norm = sqrt(norm);
+	for(int i=0; i<N; i++){
+		phi[i] /= norm;
+	}
+
+	// exporting
+	FILE *file;
+	char filename[10];
+	snprintf(filename, 10, "out%02d.dat", n);
+	printf("saving in: %s\n", filename);
+    file = fopen(filename, "w+");
+	fprintf(file, "%f\n", E1);
+	for(int i=0; i<N; i++){
+		fprintf(file, "%f %f\n", i*H, phi[i]);
+	}
+	fclose(file);
 }
 
-double integ(double E, bool save){
+void integ(double E, double *phi){
     
     double y1 = 0, y2 = H;
-    for(double x = XMAX; x > 0; x -= H){
-        numerov(&y1, &y2, x, E);
-        
-		if(save){
-			fprintf(file, "%f %f \n", x, y1);
-		}
+    for(int i=0; i < N; i++){
+        numerov(&y1, &y2, i * H, E);
+        phi[i] = y2;
     }
-	return y2;
 }
 
 void numerov(double *y1, double *y2, double x, double E){
